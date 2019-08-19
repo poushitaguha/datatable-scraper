408ecbd411b6b134029c5a975df6b0c6cb43cd95
poushitaguha <pguha.kolkata@gmail.com>
2019-08-18 23:56:51 -0400

    Refactor code to make it readable

--- index.js
+++ index.js
@@ -1,55 +1,54 @@
 const Nightmare = require("nightmare");
-const nightmare = Nightmare( {show:true} );
+const nightmare = Nightmare({ show: true });
 const createCsvWriter = require("csv-writer").createObjectCsvWriter;
 
 nightmare
-  .goto('https://www.google.com')
-  .type('input[title="Search"]', 'datatables')
-  .wait('input[type="submit"]')
-  .click('input[type="submit"]')
-  .evaluate(() => document.querySelector('.rc .r a').click())
-  .wait('select[name="example_length"]')
-  .select('select[name="example_length"]', 100).wait(2000)
-  .evaluate(() => {
-        let headers = [];
-        Array.prototype.forEach.call(
-        document.querySelectorAll('#example thead tr th'),
-        element => headers.push(element.innerText)
+    .goto('https://www.google.com')
+    .type('input[title="Search"]', 'datatables')
+    .wait('input[type="submit"]')
+    .click('input[type="submit"]')
+    .evaluate(() => document.querySelector('.rc .r a').click())
+    .wait('select[name="example_length"]')
+    .select('select[name="example_length"]', 100).wait(2000)
+    .evaluate(() => {
+        let tableHeader = Array.prototype.map.call(
+            document.querySelectorAll('#example thead tr th'),
+            element => element.innerText
         );
         let tableInfo = Array.prototype.map.call(
-        document.querySelectorAll('#example tr'),
-        function(tr) {
-            let out = Array.prototype.map.call(
-            tr.querySelectorAll('td'),
-            td => td.innerHTML
-            );
-            let output = {};
-            for (i in out) {
-            output[headers[i]] = out[i];
-            }
-            return output;
-        }
-        );
-        return [headers, tableInfo];
+            document.querySelectorAll('#example tbody tr'),
+            element_tr => {
+                let rowDataArray = Array.prototype.map.call(
+                    element_tr.querySelectorAll('td'),
+                    element_td => element_td.innerText
+                );
+                let rowDataObject = {};
+                for (let i in rowDataArray) {
+                    rowDataObject[tableHeader[i]] = rowDataArray[i];
+                }
+                return rowDataObject;
+            });
+        return [tableHeader, tableInfo];
     })
-  .end()
-  .then(result => {
-        let headers = [];
-        result[0].forEach(i => {
-        let item = new Object();
-        item.id = i;
-        item.title = i;
-        headers.push(item);
+    .end()
+    .then(result => {
+        let header = [];
+        result[0].forEach(element => {
+            let item = new Object();
+            item.id = element;
+            item.title = element;
+            header.push(item);
         });
         const csvWriter = createCsvWriter({
-        path: 'out.csv',
-        header: headers
+            path: 'datatables_output.csv',
+            header: header
         });
         csvWriter
-        .writeRecords(result[1])
-        .then(() => console.log('The CSV file was written successfully'));
+            .writeRecords(result[1])
+            .then(() => console.log('The CSV file has been written successfully'));
+        console.log(result[0]);
         console.log(result[1]);
-  })
-  .catch(error => {      
+    })
+    .catch(error => {
         console.error('Search failed:', error);
-  })
+    })
