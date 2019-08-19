6bec441f2012e1d0bdcd62b6ddc50c938c56c1e8
poushitaguha <pguha.kolkata@gmail.com>
2019-08-19 00:27:51 -0400

    Add comment lines

--- index.js
+++ index.js
@@ -3,18 +3,20 @@ const nightmare = Nightmare({ show: true });
 const createCsvWriter = require("csv-writer").createObjectCsvWriter;
 
 nightmare
-    .goto('https://www.google.com')
-    .type('input[title="Search"]', 'datatables')
+    .goto('https://www.google.com')     // Navigate to Google URL
+    .type('input[title="Search"]', 'datatables')    // Search for 'datatables' in Google
     .wait('input[type="submit"]')
     .click('input[type="submit"]')
-    .evaluate(() => document.querySelector('.rc .r a').click())
+    .evaluate(() => document.querySelector('.rc .r a').click())     // Select the first result (https://datatables.net) and click on it
     .wait('select[name="example_length"]')
-    .select('select[name="example_length"]', 100).wait(2000)
+    .select('select[name="example_length"]', 100).wait(2000)    // Filter table entries by 100 and wait for 2 secs
     .evaluate(() => {
+        // Populate table header into array
         let tableHeader = Array.prototype.map.call(
             document.querySelectorAll('#example thead tr th'),
             element => element.innerText
         );
+        // Populate table row data into array
         let tableInfo = Array.prototype.map.call(
             document.querySelectorAll('#example tbody tr'),
             element_tr => {
@@ -23,6 +25,7 @@ nightmare
                     element_td => element_td.innerText
                 );
                 let rowDataObject = {};
+                // Convert row data into an array of objects
                 for (let i in rowDataArray) {
                     rowDataObject[tableHeader[i]] = rowDataArray[i];
                 }
@@ -33,16 +36,19 @@ nightmare
     .end()
     .then(result => {
         let header = [];
+        // Create table header for CSV file
         result[0].forEach(element => {
             let item = new Object();
             item.id = element;
             item.title = element;
             header.push(item);
         });
+        // Create CSV file path and write table header to CSV file
         const csvWriter = createCsvWriter({
             path: 'datatables_output.csv',
             header: header
         });
+        // Write table row data to CSV file
         csvWriter
             .writeRecords(result[1])
             .then(() => console.log('The CSV file has been written successfully'));
